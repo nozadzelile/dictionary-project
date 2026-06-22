c6fd05779d38b917150e4f825d53d6c1c4a41244
nozadzelile <nozadzelile1@gmail.com>
2026-06-22 15:25:30 +0200

    added Header andd search.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary.js";
+import bookImg from './book.png';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <h1>DICTIONARY</h1>
+        <img src={bookImg} alt="Book" className="book-image" />
+        <h2>Search for a word</h2>
+      <main>
+        <Dictionary />
+      </main>
+      </div>
+      <footer>Coded by <span className="myName">Lile Nozadze</span></footer>
     </div>
   );
 }
