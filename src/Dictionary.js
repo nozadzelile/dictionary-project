372ee695bcfda3ac40533f028893357c6c6ecdcf
nozadzelile <nozadzelile1@gmail.com>
2026-06-22 22:56:49 +0200

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results"
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(" ");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
         console.log(response.data);
+        setResults(response.data);
     }
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`)
 
         let apiKey = "bc0c2310f91t07094a4ao08773084492";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
@@ -27,6 +29,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
             <input type="search" autoFocus={true} onChange={handleKeyword} />
             </form>
+            <Results results={results} />
         </div>
     )}
  
