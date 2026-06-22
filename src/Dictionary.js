fae85cc4114b55dc3d45b1c576122772a137c652
nozadzelile <nozadzelile1@gmail.com>
2026-06-22 20:07:29 +0200

    added API for dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import "./Dictionary.css"
+import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(" ");
 
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
     function search(event) {
         event.preventDefault();
         alert(`Searching for ${keyword}`)
+
+        let apiKey = "bc0c2310f91t07094a4ao08773084492";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeyword(event) {
