59bf43774e6603e99f3b63f0cc0d31ddfe01d04d
nozadzelile <nozadzelile1@gmail.com>
2026-06-23 12:32:59 +0200

    added Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Synonyms from "./Synonyms"
 
 export default function Meaning(props) {
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
             
-            <p>{props.meaning.definition}</p>
+            <p><strong>Definition: </strong>{props.meaning.definition}</p>
          
-            <p>{props.meaning.example}</p>
-        
+            <p><strong>Example: </strong>{props.meaning.example}</p>
+           <strong>Synonyms: </strong> 
+           <Synonyms synonym={props.meaning.synonyms} />
         </div>
     );
 }
--- src/Results.js
+++ src/Results.js
@@ -5,9 +5,9 @@ export default function Results(props) {
     if (props.results) {
         return (
         <div className="Results">
-            <h2>{props.results.word}</h2>
+            <h2 style={{textTransform:"capitalize"}}>{props.results.word}</h2>
             {props.results.meanings
-            .slice(0, 2)
+            .slice(0, 1)
             .map(function (meaning, index) {
                 return (
                     <div key={index}>    
