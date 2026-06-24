41ac6dd7111c529e2f74d16c18d4f962a0e2baf9
nozadzelile <nozadzelile1@gmail.com>
2026-06-24 21:10:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import Dictionary from "./Dictionary.js";
-import bookImg from './book.png';
-
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>DICTIONARY</h1>
-        <img src={bookImg} alt="Book" className="book-image" />
-        <h2>Search for a word</h2>
       <main>
         <Dictionary />
       </main>
